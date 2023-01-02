9517118ea01b503e6796470e75a53804907e0139
timothyshen <timothyshen1@hotmail.com>
2023-01-02 17:00:26 +0000

    ci: fix ci solidity version + deployed contract on Goerli

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,5 @@
 import { HardhatUserConfig } from "hardhat/types";
-import '@typechain/hardhat'
+import "@typechain/hardhat";
 import "@nomiclabs/hardhat-waffle";
 import "@nomiclabs/hardhat-etherscan";
 import "hardhat-deploy";
@@ -8,11 +8,7 @@ import { config as dotEnvConfig } from "dotenv";
 
 dotEnvConfig();
 
-const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
-const ROPSTEN_PRIVATE_KEY =
-  process.env.ROPSTEN_PRIVATE_KEY ||
-  "0xc87509a1c067bbde78beb793e6fa76530b6382a4c0241e5e4a9ec0a0f44dc0d3"; // well known private key
-const { ETHERSCAN_API_KEY } = process.env;
+const { ALCHEMY_API_KEY, GOERLI_PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 const config: HardhatUserConfig = {
   defaultNetwork: "hardhat",
@@ -24,13 +20,10 @@ const config: HardhatUserConfig = {
     ],
   },
   networks: {
-    ropsten: {
-      url: `https://ropsten.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [ROPSTEN_PRIVATE_KEY],
-    },
-    coverage: {
-      url: "http://127.0.0.1:8555",
-    },
+    goerli: {
+      url: `https://eth-goerli.alchemyapi.io/v2/${ALCHEMY_API_KEY}`,
+      accounts: [GOERLI_PRIVATE_KEY]
+    }
   },
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
